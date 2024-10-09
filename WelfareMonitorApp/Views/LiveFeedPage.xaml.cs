47e7a07ff14aeba31b13ee7dbceb6798b99f2690
Daniel Mærsk Pedersen <pedersendaniel3561@gmail.com>
2024-10-09 10:47:25 +0200

    .

--- WelfareMonitorApp/Views/LiveFeedPage.xaml.cs
+++ WelfareMonitorApp/Views/LiveFeedPage.xaml.cs
@@ -1,12 +1,15 @@
+//LiveFeedPage.xaml.cs
 using Microsoft.Maui.Controls;
+using WelfareMonitorApp.ViewModels;
 
 namespace WelfareMonitorApp.Views 
 {
     public partial class LiveFeedPage : ContentPage
     {
-        public LiveFeedPage()
+        public LiveFeedPage(LiveFeedViewModel viewModel)
         {
             InitializeComponent();
+            BindingContext = viewModel;
         }
     }
 }
